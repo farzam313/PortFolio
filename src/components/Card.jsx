9e6bed0c9f9db9d889c5afb68b45a93e9dd56a98
farzam313 <107337733+farzam313@users.noreply.github.com>
2025-06-29 13:28:29 +0530

    Mobile view Enhancmetn

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,12 +6,12 @@ const Card = ({ photo, description }) => {
   return (
     <div
       id="Card"
-      className="flex flex-row w-[100%] lg:flex-col h-[220px] lg:w-[300px] lg:h-[350px]  items-center border-1 border-green-400 rounded-lg p-4   "
+      className="flex flex-row w-[100%] lg:flex-col h-[22%] lg:w-[300px] lg:h-[350px]  items-center border-1 border-gray-400 rounded-lg my-1  "
     >
-      <img src={photo} className="h-[100px] lg:h-[200px] " />
-      <div className="flex flex-col items-center justify-between w-[100%] h-[100%] lg:h-[150px] lg:w-[300px] border-2 border-blue-600 ">
+      <img src={photo} className="w-[30%] lg:h-[200px] mx-auto " />
+      <div className="flex flex-col items-center justify-between w-[50%] h-[90%] lg:h-[150px] lg:w-[300px] mx-auto">
         <p className="lg:mt-4  lg:font-serif font-light ">{description}</p>
-        <div className="flex items-end justify-end lg:w-full w-[100%] border-amber-500 border-1 ">
+        <div className="flex items-end justify-center lg:w-full w-[100%]  ">
           <Button
             caption={"See More"}
             Icon={FaArrowRight}
