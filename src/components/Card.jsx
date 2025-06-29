544e498fe58f29cfb236c549fb5e4540c6189549
farzam313 <107337733+farzam313@users.noreply.github.com>
2025-06-29 14:14:20 +0530

    responsivness enhanced

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,14 +8,14 @@ const Card = ({ photo, description }) => {
       id="Card"
       className="flex flex-row w-[100%] lg:flex-col h-[22%] lg:w-[300px] lg:h-[350px]  items-center border-1 border-gray-400 rounded-lg my-1  "
     >
-      <img src={photo} className="w-[30%] lg:h-[200px] mx-auto " />
+      <img src={photo} className="h-[70%] lg:h-[200px] mx-auto py-3" />
       <div className="flex flex-col items-center justify-between w-[50%] h-[90%] lg:h-[150px] lg:w-[300px] mx-auto">
-        <p className="lg:mt-4  lg:font-serif font-light ">{description}</p>
-        <div className="flex items-end justify-center lg:w-full w-[100%]  ">
+        <p className="lg:mt-4  lg:font-serif font-light p-3 ">{description}</p>
+        <div className="flex items-end justify-center lg:w-full w-[100%] pb-2 ">
           <Button
             caption={"See More"}
             Icon={FaArrowRight}
-            className="flex flex-end h-[30px] text-sm"
+            className="flex flex-end h-[30px] text-sm "
           />
         </div>
       </div>
